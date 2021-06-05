1d2bfe6b70dbda52160324f51caca89de7e38d50
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-06-05 17:18:10 +0800

    variables

---
+++ variables.nim
@@ -0,0 +1,13 @@
+var weee: int = 32
+var wooo = "hey there"
+var im_special = "carcar"
+imSpecial = "carcarooooo"
+
+var
+	v1 = 1
+	v2 = 2
+	v3 = 6
+
+echo "hey, wat is ", im_special, " am i right"
+
+# nim c -r variables.nim
