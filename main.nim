fa7add896a089df85a348034258bfdf1240bc091
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-06-06 11:48:45 +0800

    read line, if statements, functions

--- main.nim
+++ main.nim
@@ -1,13 +1,40 @@
-var weee: int = 32
-var wooo = "hey there"
-var im_special = "carcar"
-imSpecial = "carcarooooo"
+#? replace(sub = "\t", by = " ")
 
-var
+const weee: int = 32
+const wooo = "hey there"
+var im_special = "carcar"
+imSpecial = "this variable"
+
+#[
+█░░ █▀▀ █▀▀█ █▀▀█ █▀▀▄ ░▀░ █▀▀▄ █▀▀▀ 　 █▀▀▄ ░▀░ █▀▄▀█ 
+█░░ █▀▀ █▄▄█ █▄▄▀ █░░█ ▀█▀ █░░█ █░▀█ 　 █░░█ ▀█▀ █░▀░█ 
+▀▀▀ ▀▀▀ ▀░░▀ ▀░▀▀ ▀░░▀ ▀▀▀ ▀░░▀ ▀▀▀▀ 　 ▀░░▀ ▀▀▀ ▀░░░▀
+]#
+
+const
 	v1 = 1
 	v2 = 2
 	v3 = 6
 
+let
+	t1 = v2 / v3 # returns float 
+	t2 = v2 div v3 # returns int (floors it)
+
 echo "hey, wat is ", im_special, " am i right"
+
+if v2 < v3 and weee > v3:
+	echo "woop woop this is an if"
+elif v2 < v3:
+	echo "some if statement"
+else:
+	echo "no if statement 4 u"
+
+# functions (called procedures)
+proc broThisIsEpicc(var1: int, var2: int): int =
+	return var1 + var2
+echo broThisIsEpicc(v1, v2)
+
+let doYouLikeToRead = readLine(stdin)
+echo doYouLikeToRead, "\n", r"\n", """\n"""
 
 # nim c -r variables.nim
